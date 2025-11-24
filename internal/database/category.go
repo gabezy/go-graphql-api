89cab72b5d33351b5a4b1fbff90018f5e182f824
gabezy <gabrielmoreira2705@gmail.com>
2025-11-23 21:19:39 -0300

    feat: add category to db via mutation and list all categories

---
+++ internal/database/category.go
@@ -0,0 +1,53 @@
+// Package database
+package database
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+type Category struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+}
+
+func NewCategory(db *sql.DB) *Category {
+	return &Category{db: db}
+}
+
+func (c *Category) Create(name string, description string) (Category, error) {
+	id := uuid.New().String()
+	_, err := c.db.Exec("INSERT INTO category (id, name, description) VALUES ($1, $2, $3)",
+		id, name, description)
+	if err != nil {
+		return Category{}, err
+	}
+
+	return Category{ID: id, Name: name, Description: description}, nil
+}
+
+func (c *Category) FindAll() ([]Category, error) {
+	var categories []Category
+
+	rows, err := c.db.Query("SELECT * FROM category")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var cat Category
+		if err := rows.Scan(&cat.ID, &cat.Name, &cat.Description); err != nil {
+			return nil, err
+		}
+		categories = append(categories, cat)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return categories, nil
+}
